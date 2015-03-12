efc864ace1041d52d5583d55a2e1eb9e68e53488
nfeb <nfeb@localhost.localdomain>
2015-03-12 15:26:07 +0900

    ! repo path prefix parameter added @ debug.sh

--- server/debug.sh
+++ server/debug.sh
@@ -1,7 +1,7 @@
-sudo mkdir -p ~/data/perforce
-sudo mkdir -p ~/var/log/perforce
-sudo chcon -Rt svirt_sandbox_file_t ~/data/perforce
-sudo chcon -Rt svirt_sandbox_file_t ~/var/log/perforce
-sudo docker stop perforce_server_debug
-sudo docker rm perforce_server_debug
-sudo docker run -it --name perforce_server_debug -p 1666:1666 -v ~/data/perforce:/data/perforce -v ~/var/log/perforce:/var/log/perforce centos-perforce-server /bin/bash
+sudo mkdir -p $1/data/perforce
+sudo mkdir -p $1/var/log/perforce
+sudo chcon -Rt svirt_sandbox_file_t $1/data/perforce
+sudo chcon -Rt svirt_sandbox_file_t $1/var/log/perforce
+sudo docker stop perforce_server
+sudo docker rm perforce_server
+sudo docker run -it --name perforce_server -p 1666:1666 -v $1/data/perforce:/data/perforce -v $1/var/log/perforce:/var/log/perforce centos-perforce-server /bin/bash
